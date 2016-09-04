acbdf5ac092d15e926670924d16e7c912e7b86bc
daryl <daryl.mathison@gmail.com>
2016-09-04 03:15:49 -0500

    added search form for github searches

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,16 @@
 from flask import render_template
 from flask import Blueprint
+from .forms import SearchForm
+from .service import GithubUserService
 
 main_app = Blueprint('main_app', __name__)
 
-@main_app.route('/', methods=("GET",))
+@main_app.route('/', methods=("GET","POST"))
 def hello_world():
-    return render_template("index.html", name="Daryl")
+    form = SearchForm()
+    results = []
+    if form.validate_on_submit():
+        name = form.username.data
+        results = GithubUserService.search_for_user(name)
+
+    return render_template("index.html", results=results, form=form)
