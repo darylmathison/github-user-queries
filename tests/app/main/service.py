2a045fc62c86564aed806cd79e1f0caa44f64592
daryl <daryl.mathison@gmail.com>
2016-11-27 15:40:18 -0600

    test:(tests.app.main.service): added fork without pr and added non_fork without pr and with pr

--- tests/app/main/service.py
+++ tests/app/main/service.py
@@ -1,11 +1,39 @@
 import unittest
 from unittest.mock import patch
 from app.main.service import GitHubUserService
-from app.main.model import FoundGitHubUser
 
 
 @patch("app.main.service.github")
 class TestGitHubUserService(unittest.TestCase):
+
+    def setUp(self):
+        self.test_user = "test"
+        self.retrieved_repos_return = [
+            {
+                "fork": False,
+                "name": "test_non_fork",
+                "pull_url": "http://localhost/non_fork/pulls",
+                "url": "https://localhost/non_fork",
+                "full_name": self.test_user + "/test_non_fork",
+                "html_url": "https://localhost"
+            },
+            {
+                "fork": True,
+                "name": "test_fork",
+                "full_name": self.test_user + "/test_fork",
+                "url": "https://localhost/child",
+                "html_url": "https://localhost",
+                "parent": {
+                    "fork": False,
+                    "name": "parent",
+                    "url": "http://parent",
+                    "full_name": self.test_user + "1/test_parent",
+                    "pull_url": "https://localhost/parent/pulls",
+                    "html_url": "https://localhost/parent"
+                }
+            }
+        ]
+
     def test_search_for_users_error(self, github_client):
         message = "too many"
         github_client.search_for_user.return_value = {"error": message}
@@ -26,62 +54,27 @@ class TestGitHubUserService(unittest.TestCase):
         self.assertEqual(found_users[0].login, github_client_return[0]["login"])
 
     def test_retrieve_repos_if_fork_with_pr(self, github_client):
-        test_user = "test_user"
-        retrieved_repos_return = [
-            {
-                "fork": False,
-                "name": "test_non_fork",
-                "pull_url": "http://localhost/non_fork/pulls",
-                "url": "https://localhost/non_fork",
-                "full_name": test_user + "/test_non_fork",
-                "html_url": "https://localhost"
-            },
-            {
-                "fork": True,
-                "name": "test_fork",
-                "full_name": test_user + "/test_fork",
-                "url": "https://localhost/child",
-                "html_url": "https://localhost",
-                "parent": {
-                    "fork": False,
-                    "name": "parent",
-                    "url": "http://parent",
-                    "full_name": test_user + "1/test_parent",
-                    "pull_url": "https://localhost/parent/pulls",
-                    "html_url": "https://localhost/parent"
-                }
-            }
-        ]
-
-        def mock_retrieve_repo(url):
-            if "non_fork" in url:
-                return retrieved_repos_return[0]
-            elif "parent" in url:
-                return retrieved_repos_return[1]["parent"]
-            else:
-                return retrieved_repos_return[1]
-
-        def mock_retrieve_pulls(url, state):
+        def local_mock_retrieve_pulls(url, state):
             pulls = [
                 {
                     "html_url": "https://localhost/parent/pulls",
                     "title": "test title",
                     "user": {
-                        "login": test_user
+                        "login": self.test_user
                     }
                 }
             ]
             if "parent" in url:
                 return pulls
             else:
-                pulls[0]["html_url"] = retrieved_repos_return[0]["html_url"]
+                pulls[0]["html_url"] = self.retrieved_repos_return[0]["html_url"]
             return pulls
         # mocks
-        github_client.retrieve_repos.return_value = retrieved_repos_return
-        github_client.retrieve_repo.side_effect = mock_retrieve_repo
-        github_client.retrieve_pulls.side_effect = mock_retrieve_pulls
+        github_client.retrieve_repos.return_value = self.retrieved_repos_return
+        github_client.retrieve_repo.side_effect = self.mock_retrieve_repo
+        github_client.retrieve_pulls.side_effect = local_mock_retrieve_pulls
 
-        actual_repos = GitHubUserService.retrieve_repos(test_user)
+        actual_repos = GitHubUserService.retrieve_repos(self.test_user)
         self.assertEqual(2, len(actual_repos))
         for repo in actual_repos:
             if repo.is_fork:
@@ -89,6 +82,107 @@ class TestGitHubUserService(unittest.TestCase):
                                 repo.pull_requests[0].url,
                                 "The parent pulls are not in the repo: {}"
                                 .format(repo.name))
+
+    def test_retrieve_repos_if_fork_without_pr(self, github_client):
+        def local_mock_retrieve_pulls(url, state):
+            pulls = [
+                {
+                    "html_url": "https://localhost/parent/pulls",
+                    "title": "test title",
+                    "user": {
+                        "login": self.test_user
+                    }
+                }
+            ]
+            if "parent" in url:
+                return []
+            else:
+                pulls[0]["html_url"] = self.retrieved_repos_return[0]["html_url"]
+            return pulls
+
+        # mocks
+        github_client.retrieve_repos.return_value = self.retrieved_repos_return
+        github_client.retrieve_repo.side_effect = self.mock_retrieve_repo
+        github_client.retrieve_pulls.side_effect = local_mock_retrieve_pulls
+
+        actual_repos = GitHubUserService.retrieve_repos(self.test_user)
+        for repo in actual_repos:
+            if repo.is_fork:
+                self.assertIsNone(repo.pull_requests,
+                                  "The parent pulls are not in the repo: {}"
+                                  .format(repo.name))
+
+    def test_retrieve_repos_if_source_with_pr(self, github_client):
+        def local_mock_retrieve_pulls(url, state):
+            pulls = [
+                {
+                    "html_url": "https://localhost/non_fork/pulls",
+                    "title": "test title",
+                    "user": {
+                        "login": self.test_user
+                    }
+                }
+            ]
+            return pulls
+
+        # mocks
+        github_client.retrieve_repos.return_value = self.retrieved_repos_return
+        github_client.retrieve_repo.side_effect = self.mock_retrieve_repo
+        github_client.retrieve_pulls.side_effect = local_mock_retrieve_pulls
+
+        actual_repos = GitHubUserService.retrieve_repos(self.test_user)
+        self.assertEqual(2, len(actual_repos))
+
+        for repo in actual_repos:
+            if not repo.is_fork:
+                self.assertTrue("non_fork" in
+                                repo.pull_requests[0].url,
+                                "The non_fork pulls are not in the repo: {}"
+                                .format(repo.name))
+
+    def test_retrieve_repos_if_source_without_pr(self, github_client):
+            def local_mock_retrieve_pulls(url, state):
+                return []
+
+            # mocks
+            github_client.retrieve_repos.return_value = self.retrieved_repos_return
+            github_client.retrieve_repo.side_effect = self.mock_retrieve_repo
+            github_client.retrieve_pulls.side_effect = local_mock_retrieve_pulls
+
+            actual_repos = GitHubUserService.retrieve_repos(self.test_user)
+            self.assertEqual(2, len(actual_repos))
+
+            for repo in actual_repos:
+                if not repo.is_fork:
+                    self.assertIsNone(repo.pull_requests,
+                                      "The non_fork pulls are not in the repo: {}"
+                                      .format(repo.name))
+
+    # -----------------helper mock functions--------------------
+
+    def mock_retrieve_repo(self, url):
+        if "non_fork" in url:
+            return self.retrieved_repos_return[0]
+        elif "parent" in url:
+            return self.retrieved_repos_return[1]["parent"]
+        else:
+            return self.retrieved_repos_return[1]
+
+    def mock_retrieve_pulls(self, url, state):
+        pulls = [
+            {
+                "html_url": "https://localhost/parent/pulls",
+                "title": "test title",
+                "user": {
+                    "login": self.test_user
+                }
+            }
+        ]
+        if "parent" in url:
+            return pulls
+        else:
+            pulls[0]["html_url"] = self.retrieved_repos_return[0]["html_url"]
+        return pulls
 
 if __name__ == '__main__':
     unittest.main()
