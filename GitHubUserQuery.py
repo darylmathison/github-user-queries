90155a4b3ee26e22b860b0a69dd6d13690fd343d
darylmathison <daryl.mathison@gmail.com>
2019-07-18 09:12:54 -0500

    Created a better secret.

--- GitHubUserQuery.py
+++ GitHubUserQuery.py
@@ -1,4 +1,4 @@
 from app import create_app
 
 app = create_app()
-app.config["SECRET_KEY"] = "kjdfks;lkfa;fkasflkklkkkkdkdkdkdkdsk"
+app.config["SECRET_KEY"] = b"\t\xa8\xd0\x80\xa0Pq*\xcbi\x90l qx."
