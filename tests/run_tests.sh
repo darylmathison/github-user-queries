4a69941d7ba7ca567754cb05b7f0ab9b1f131351
darylmathison <daryl.mathison@gmail.com>
2019-08-01 01:17:59 -0500

    Added confidence level
    Refers-to: #22

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 pip install bandit
-bandit -ll -r GitHubUserQuery.py app && python -m unittest discover -s tests -t .
+bandit -ll -ii -r GitHubUserQuery.py app && python -m unittest discover -s tests -t .
